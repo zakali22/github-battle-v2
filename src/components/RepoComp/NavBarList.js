4d2f85131dbf75f86fea8dc462161cd7ca71596c
Zakeria Ali <zak.fined@gmail.com>
2020-09-20 00:37:27 +0100

    Add theme to player review and repos result components

--- src/components/RepoComp/NavBarList.js
+++ src/components/RepoComp/NavBarList.js
@@ -1,17 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {ThemeConsumer} from "../../context/ThemeContext"
 
 const NavBarList = ({links, currNavSelected, handleNavSelectedChange}) => {
     return (
-        <ul className="navbar">
-            {links.map((link, id) => {
-                return (
-                    <li key={id}>
-                        <button className={`${currNavSelected === link ? 'active' : null}`} onClick={() => handleNavSelectedChange(link)}>{link}</button>
-                    </li>
-                )
-            })}
-        </ul>
+        <ThemeConsumer>
+            {({theme}) => (
+                <ul className={`navbar ${theme === 'dark' ? theme : ''}`}>
+                    {links.map((link, id) => {
+                        return (
+                            <li key={id}>
+                                <button className={`${currNavSelected === link ? 'active' : null}`} onClick={() => handleNavSelectedChange(link)}>{link}</button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
+        </ThemeConsumer>
     )
 }
 
--- src/components/RepoGrid/RepoGrid.js
+++ src/components/RepoGrid/RepoGrid.js
@@ -1,26 +1,31 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from "react-icons/fa"
+import {ThemeConsumer} from "../../context/ThemeContext"
 
 const RepoGrid = ({repos}) => {
     return (
-        <ul className="repo-grid">
-            {repos.map((repo, id) => {
-                return (
-                    <li key={id} className="repo-grid__item">
-                        <p>#{id+1}</p>
-                        <img src={repo.owner.avatar_url} alt="Repo" className="repo-grid__item--logo" />
-                        <p className="repo-grid__item--headline-text">{repo.name}</p>
-                        <div className="repo-grid__item--desc">
-                            <p><FaUser color="rgb(255, 191, 116)" size={22}/> {repo.name}</p>
-                            <p><FaStar color="rgb(255, 215, 0)" size={22}/> {repo.stargazers_count} stars</p>
-                            <p><FaCodeBranch color="rgb(129, 195, 245)" size={22}/> {repo.forks_count} forks</p>
-                            <p><FaExclamationTriangle color="rgb(241, 138, 147)" size={22}/> {repo.open_issues_count} open issues</p>
-                        </div>
-                    </li>
-                )
-            })}
-        </ul>
+        <ThemeConsumer>
+            {({theme}) => (
+                <ul className="repo-grid">
+                    {repos.map((repo, id) => {
+                        return (
+                            <li key={id} className={`repo-grid__item ${theme === 'dark' ? theme : ''}`}>
+                                <p>#{id+1}</p>
+                                <img src={repo.owner.avatar_url} alt="Repo" className="repo-grid__item--logo" />
+                                <p className="repo-grid__item--headline-text">{repo.name}</p>
+                                <div className="repo-grid__item--desc">
+                                    <p><FaUser color="rgb(255, 191, 116)" size={22}/> {repo.name}</p>
+                                    <p><FaStar color="rgb(255, 215, 0)" size={22}/> {repo.stargazers_count} stars</p>
+                                    <p><FaCodeBranch color="rgb(129, 195, 245)" size={22}/> {repo.forks_count} forks</p>
+                                    <p><FaExclamationTriangle color="rgb(241, 138, 147)" size={22}/> {repo.open_issues_count} open issues</p>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
+        </ThemeConsumer>
     )
 }
 
